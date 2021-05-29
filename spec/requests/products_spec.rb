9a8d1a01123dcc164914c377321529f201bd5056
poojasax <pooja.saxena@efi.com>
2021-05-29 22:31:45 +0530

    Added procilfe for db migrations on heroku and changed root API

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,10 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Products", type: :request do
   describe "GET /products" do
-    it "works! (now write some real specs)" do
-      get products_path
+    it "should return products list" do
+      get "/products"
       expect(response).to have_http_status(200)
     end
   end
+
+  # describe "POST /products" do
+  #   it "should not create a new product" do
+  #     get "/products"
+  #     expect(response).to have_http_status(422)
+  #   end
+
+  #   it "should create new product" do
+  #     get "/products"
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+  # describe "PUT /products/:id" do
+  #   it "should give error while updating an existing products" do
+  #     get "/products"
+  #     expect(response).to have_http_status(200)
+  #   end
+
+  #   it "should update an existing products" do
+  #     get "/products"
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+  # describe "GET /product/:id" do
+  #   it "should return products list" do
+  #     get "/products"
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 end
